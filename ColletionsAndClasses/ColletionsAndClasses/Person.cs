8928eea4f1a7940827d1466425ca48506551ccf7
Admin <B1a2k3o4@gmail.com>
2024-10-07 21:40:59 +0400

    ძველი ფოლდერები წავშალე

---
+++ ColletionsAndClasses/ColletionsAndClasses/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColletionsAndClasses
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        private int age;
+        public int Age {  get
+            {
+                return age;
+            }
+            set
+            { if (value < 0)
+
+                {
+                    age = 0;
+                }
+                else if (value > 120)
+                {
+                    age = 120;
+                }
+                else
+                {
+                    age = value;
+                }
+            } 
+        }   
+        
+            
+        
+
+        public string DisplayInfo()
+        {
+            if (Name != null)
+
+            { return $" Name : {Name}, Age : {Age} "; }
+
+            else { return " enter both name and age "; }
+        }
+    }
+}
---
+++ ColletionsAndClasses/ColletionsAndClasses/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColletionsAndClasses
+{
+    internal class Product : IProduct
+    {
+        public string Name { get; set; }
+        private decimal price;
+        public decimal Price 
+        {
+            get
+            { 
+                return price;
+            }
+            set 
+            {
+                if (value < 0)
+                {
+                    price = 0;
+                }
+                else
+                {
+                    price = value;
+                }
+            } 
+        }
+        public enum Currency { GEL, USD, EUR }
+        
+        private Currency _currency;
+        public Currency currency
+        { 
+            get 
+            {
+                return _currency; 
+            } set
+            {
+                _currency = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $" product : {Name}, Price : {Price}, Currency {currency} ";
+        }
+
+
+    }
+}
