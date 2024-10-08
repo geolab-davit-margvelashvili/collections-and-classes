ca2ee30bee192b3149d35d038c905975863e7163
Nika Kharatishvili <daimlernika@gmail.com>
2024-10-08 16:30:08 +0400

    Task two Car class

---
+++ ColletionsAndClasses/ColletionsAndClasses/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColletionsAndClasses
+{
+    internal class Car
+    {
+        public string Brand { get; set; }
+        private int _year;
+        private int firstCarYear = 1886;
+        private int currentYear = DateTime.Now.Year;
+        public int Year
+        {
+            get { return _year; }
+            set
+            {
+                if (value < firstCarYear)
+                {
+                    _year = firstCarYear;
+                }
+                else if (value > currentYear)
+                {
+                    _year = currentYear;
+                }
+                else
+                {
+                    _year = value;
+                }
+            }
+
+        }
+    }
+}
